e3da147f614c358e9785d2c8be201888f2fc2fa2
leenabaxioned <leenab@axioned.com>
2024-05-30 12:44:24 +0530

    structure added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,19 @@
 /* Author: */
+var input = document.querySelector("#word-input");
+input.addEventListener('keypress',createButtons);
 
+function createButtons() {
+  var inputValue = document.getElementById("word-input").value;
+  var words = inputValue.split(",");
+  console.log(words);
+  var container = document.getElementById("buttons-container");
+  container.innerHTML = "";
+
+  words.forEach(function(word) {
+    var button = document.createElement("button");
+    button.textContent = word.trim();
+    console.log(button.textContent);
+    button.className = "word-button";
+    container.appendChild(button);
+  });
+}
